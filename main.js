ccd0a4ed5c9f23b239edf84338b455f9f18ee6be
chunjull <chunjuchein@gmail.com>
2023-11-17 19:53:04 +0800

    refactor: events & issues section

--- main.js
+++ main.js
@@ -2,6 +2,15 @@ import './assets/scss/all.scss';
 
 console.log("Hello world!");
 
+const len = 75;
+const ellipsis = document.querySelectorAll('.ellipsis');
+ellipsis.forEach((item) => {
+  if(item.innerHTML.length > len) {
+    let txt = item.innerHTML.substring(0, len) + '...';
+    item.innerHTML = txt;
+  }
+})
+
 var triggerTabList = [].slice.call(document.querySelectorAll('#myTab button'))
 triggerTabList.forEach(function (triggerEl) {
   var tabTrigger = new bootstrap.Tab(triggerEl)
