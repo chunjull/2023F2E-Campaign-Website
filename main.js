ca6d328df70b12e9ade63eebd42e044898546bea
chunjull <chunjuchein@gmail.com>
2023-11-17 16:00:40 +0800

    refactor: header

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
 import './assets/scss/all.scss';
 
 console.log("Hello world!");
+
+var triggerTabList = [].slice.call(document.querySelectorAll('#myTab button'))
+triggerTabList.forEach(function (triggerEl) {
+  var tabTrigger = new bootstrap.Tab(triggerEl)
+
+  triggerEl.addEventListener('click', function (event) {
+    event.preventDefault()
+    tabTrigger.show()
+  })
+})
